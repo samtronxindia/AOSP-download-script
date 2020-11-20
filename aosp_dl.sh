60c776db437fb3e40121178229df4bcc194f1bc8
samtronxindia <samir.talegaon@utsa.edu>
2020-11-20 07:52:11 -0600

    first commit

--- aosp_dl.sh
+++ aosp_dl.sh
@@ -5,8 +5,8 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 mkdir WORKING_DIRECTORY2
 cd WORKING_DIRECTORY2
-git config --global user.name "ST"
-git config --global user.email "samtronxindia@gmail.com"
+git config --global user.name "your user name"
+git config --global user.email "youremail@mailprovider<dot>com"
 repo init -u https://android.googlesource.com/platform/manifest
 repo sync 
 . build/envsetup.sh
